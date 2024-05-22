6cdd8e8d3cbaae17643f40ff622130c2d399b53f
Ju-lmtre <135603237+Ju-lmtre@users.noreply.github.com>
2024-05-22 17:17:51 +0200

    Pseudo de la mort

--- remoteServer.js
+++ remoteServer.js
@@ -2,11 +2,17 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var count = 0
+var count = 0;
 
 app.get('/', function(req, res){
 	count++;
 	res.sendFile(__dirname + '/index.html');
+	//
+});
+
+app.get('/pagaie.html', function(req, res){
+	count++;
+	res.sendFile(__dirname + '/pagaie.html');
 	//
 });
 
@@ -23,23 +29,21 @@ io.on('connection', function(socket){
   socket.userId = userId ++;
   console.log('a user connected, user id: ' + socket.userId);
 
-  socket.on('spawn', function(msg){
+  socket.on('spawn', function(pseudo){
 		//msg = JSON.parse(msg);
-		console.log('message from user ' + msg.pseudo);
-		io.emit("spawn",msg.pseudo);
+		console.log('message from user ' + pseudo);
+		io.emit("spawn",pseudo);
   });
   
   socket.on('input1', function(msg){
-		console.log(msg);
 		//msg = JSON.parse(msg);
-		console.log("a "+msg.pseudo+" user has pressed input1");
-		io.emit("input1",msg.pseudo);
+		console.log("a "+msg+" user has pressed input1");
+		io.emit("input1",msg);
   });
   
   socket.on('input2', function(msg){
-		//msg = JSON.parse(msg);
-		console.log('user ' + msg.pseudo+" has pressed input2");
-		io.emit("input2",msg.pseudo);
+		console.log('user ' + msg+" has pressed input2");
+		io.emit("input2",msg);
   });
   
 });
